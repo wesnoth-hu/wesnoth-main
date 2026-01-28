d0b7a7466fd53d93fc331c7f708d0bdbcdd827d9
Paul Andrei Ciocan <ciocanpa87@gmail.com>
2026-01-28 11:16:06 +0100

    WN-40 chore(setup): add mdx and Version component, start integrating Auth UI into Frontend

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
